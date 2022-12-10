01abf8959eedc02d5c64136acc61654baa0b08c8
Fabricio Miranda <fbrnery@gmail.com>
2022-12-10 19:56:11 -0300

    Gerei scaffold/tabela pedidos e pedidos/produtos

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :pedido_produtos
+  resources :pedidos
   resources :receita
   resources :despesas
   resources :fornecedors
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_10_200046) do
+ActiveRecord::Schema.define(version: 2022_12_10_214620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,24 @@ ActiveRecord::Schema.define(version: 2022_12_10_200046) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "pedido_produtos", force: :cascade do |t|
+    t.bigint "pedido_id", null: false
+    t.bigint "produto_id", null: false
+    t.float "valor"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pedido_id"], name: "index_pedido_produtos_on_pedido_id"
+    t.index ["produto_id"], name: "index_pedido_produtos_on_produto_id"
+  end
+
+  create_table "pedidos", force: :cascade do |t|
+    t.bigint "cliente_id", null: false
+    t.float "valor_total"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["cliente_id"], name: "index_pedidos_on_cliente_id"
+  end
+
   create_table "produtos", force: :cascade do |t|
     t.string "nome"
     t.text "descricao"
@@ -59,4 +77,7 @@ ActiveRecord::Schema.define(version: 2022_12_10_200046) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "pedido_produtos", "pedidos"
+  add_foreign_key "pedido_produtos", "produtos"
+  add_foreign_key "pedidos", "clientes"
 end
