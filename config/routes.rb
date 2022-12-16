27a9712bd3394e1c1a86324a79f7fe118516d464
Fabricio Miranda <fbrnery@gmail.com>
2022-12-16 13:48:05 -0300

    Gerei uma sessão administrativa com login e senha de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  get 'admin/index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
   resources :faturamentos
+  
   resources :despesas
   resources :pedidos do
     resources :pedido_produtos
@@ -9,7 +22,7 @@ Rails.application.routes.draw do
   resources :produtos
   resources :clientes
   root 'home#index'
-  get 'caixa/index'
   get 'home/index'
+  get 'caixa/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_15_124005) do
+ActiveRecord::Schema.define(version: 2022_12_16_135912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,13 @@ ActiveRecord::Schema.define(version: 2022_12_15_124005) do
     t.datetime "vencimento"
     t.float "preco_compra"
     t.float "preco_venda"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
